a7483e13d78cced5b29d155f314020eacfdcd00b
YulietB <162941294+YulietB@users.noreply.github.com>
2024-05-08 21:40:04 -0600

    Add files via upload

---
+++ Proyecto_P2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "") // lanza un mensaje de texto si no estan completo los datos y para guardar
+            {
+                MessageBox.Show("Los datos no han sido completados");
+            }
+            else
+            {
+                MessageBox.Show("Tus datos han sido guardados <3");
+            }// esto se hace de tarea todos los valores
+            // deben de almacenarse en su
+            // caja de memoria correspondiente
+            // usando la clase hija que corresponda
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); //sirve para limpiar la pantalla de cada caja de texto
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();// se hace de tarea
+            // debe de terminar la ejecucion de la interfaz
+        }
+    }
+}
---
+++ Proyecto_P2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    class Persona //clase padre
+    {
+        protected string NombreCompleto;
+        protected string FechaN;
+        protected int Edad;
+        protected int DNI_Matricula;
+        protected string Carrera_Puesto;
+
+        public string nombrecompleto //Caja de memoria
+        {
+            get { return NombreCompleto; }
+            set { NombreCompleto = value; }
+        }
+        public string fechan
+        {
+            get { return FechaN; }
+            set { FechaN = value; }
+        }
+        public int edad
+        {
+            get { return Edad; }
+            set { Edad = value; }
+        }
+        public int dni_matricula
+        {
+            get { return DNI_Matricula; }
+            set { DNI_Matricula = value; }
+        }
+        public string carrera_Puesto
+        {
+            get { return Carrera_Puesto; }
+            set { Carrera_Puesto = value; }
+        }
+
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+        //aqui va el constructor para cargar datos en las cajas de memoria
+        //nombre, edad, fecha de nacimiento(es un tipo cadena!!!!! string)
+    }
+
+    class Alumno :Persona //clase hija alumno
+    {
+     //hola como esta? soy pablo y le estoy haciendo el codigo a esta niña xd  
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+    class Empleado : Persona //clase hija
+    {
+        protected int Sueldo;
+        public int sueldo
+        {
+            get { return Sueldo; }
+            set { Sueldo = value; }
+        }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+
+    class Docente : Persona //clase Hija
+    {
+        protected int Sueldo;
+        public int sueldo
+        {
+            get { return Sueldo; }
+            set { Sueldo = value; }
+        }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
